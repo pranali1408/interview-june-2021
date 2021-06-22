9588e9caa850c10cf9bc98aaf40bb087be918705
Tejz0560 <59393684+Tejz0560@users.noreply.github.com>
2021-06-22 07:18:21 +0530

    Answers

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,28 @@
-package exam;
-
-public class ShiftElementsInArray {
-
-  public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
-  }
+import java.util.*;
+class ShiftElementsInArray {
+    public static void main(String[] args) {    
+        int [] arr = new int [10] ;
+        Scanner sc=new Scanner(System.in); 
+        for(int i=0; i<10; i++)  
+       {   
+              arr[i]=sc.nextInt();  
+         }    
+        int n = 1;     
+        for(int i = 0; i < n; i++){  
+            int j, last;  
+            last = arr[arr.length-1];  
+          
+            for(j = arr.length-1; j > 0; j--){   
+                arr[j] = arr[j-1];  
+            }  
+            arr[0] = last;  
+        }  
+      
+        System.out.println();  
+        for(int i = 0; i< arr.length; i++){  
+            System.out.print(arr[i] + " ");  
+        }  
+     
+}  
 
 }
