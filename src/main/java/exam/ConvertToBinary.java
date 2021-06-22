963101b65f78aa2bf88f47586ec872806af04449
Tejz0560 <59393684+Tejz0560@users.noreply.github.com>
2021-06-22 07:23:40 +0530

    Revert "Answers"
    This reverts commit 9588e9caa850c10cf9bc98aaf40bb087be918705.

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,22 +1,9 @@
-import java.util.*; 
-class ConvertToBinary {
+package exam;
 
-  static void toBinary(int n) {
-    int[] binaryNum = new int[32];
-    int i = 0;
-    while (n > 0) {
-    	binaryNum[i] = n % 2;
-	n = n / 2;
-	i++;
-    }
-    for (int j = i - 1; j >= 0; j--)
-	System.out.print(binaryNum[j]);
-  
+public class ConvertToBinary {
+
+  public String toBinary(final int n) {
+    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    return "";
   }
-  public static void main(String[] args)
-	{
-		Scanner sc= new Scanner(System.in); 
-		int a= sc.nextInt();  
-		toBinary(a);
-	}
 }
